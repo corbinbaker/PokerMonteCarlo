f08e145e94273b57f9ef1ca626cac73cc88d6f86
corbinbaker <corbinbaker@mymail.mines.edu>
2018-04-18 22:14:46 -0600

    bethistory finished

--- PokerMonteCarlo/PokerMonteCarlo/BetHistory.cpp
+++ PokerMonteCarlo/PokerMonteCarlo/BetHistory.cpp
@@ -1,23 +1,26 @@
 #include "BetHistory.h"
 #include "Bet.h"
+#include <vector>
 
 
 class BetHistory
 {
+	vector<Bet> bhistory;
+public:
 	void clearHistory() {
-
+		bhistory.clear();
 	}
 
 	void addBet(Bet bet) {
-
+		bhistory.push_back(bet);
 	}
 
 	int getCount() {
-
+		return bhistory.size();
 	}
 
 	Bet getBet(int n) {
-
+		return bhistory[n];
 	}
 
 };
--- PokerMonteCarlo/PokerMonteCarlo/BetHistory.h
+++ PokerMonteCarlo/PokerMonteCarlo/BetHistory.h
@@ -1,12 +1,12 @@
 #pragma once
+using namespace std;
 class BetHistory
 {
+	vector<Bet> bhistory;
+public:
 	void clearHistory();
-
 	void addBet(Bet bet);
-
 	int getCount();
-
 	Bet getBet(int n);
 
 };
