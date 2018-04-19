f2a50f1cfee0a78fccdace8a05f560cf7e5070c3
corbinbaker <corbinbaker@mymail.mines.edu>
2018-04-18 22:22:10 -0600

    player finished, GETBET NEEDED

--- PokerMonteCarlo/PokerMonteCarlo/Player.cpp
+++ PokerMonteCarlo/PokerMonteCarlo/Player.cpp
@@ -4,32 +4,38 @@
 
 class Player
 {
+	Hand pHand;
+	BetHistory bhistory;
+	int pID;
+	int pChips;
+
+public:
 	int getBet(Hand opponent, BetHistory bh, int bet2player, bool canRaise, int pot) {
 
 	}
 
 	void setID(int id) {
-
+		pID = id;
 	}
 
 	int getID() {
-
+		return pID;
 	}
 
 	void setHand(Hand hand) {
-
+		pHand = hand;
 	}
 
 	Hand getHand() {
-
+		return pHand;
 	}
 
 	void setChips(int chips) {
-
+		pChips = chips;
 	}
 
 	int getChips() {
-
+		return pChips;
 	}
 
 };
--- PokerMonteCarlo/PokerMonteCarlo/Player.h
+++ PokerMonteCarlo/PokerMonteCarlo/Player.h
@@ -1,6 +1,12 @@
 #pragma once
 class Player
 {
+	Hand pHand;
+	BetHistory bhistory;
+	int pID;
+	int pChips;
+
+public:
 	int getBet(Hand opponent, BetHistory bh, int bet2player, bool canRaise, int pot);
 	void setID(int id);
 	int getID();
