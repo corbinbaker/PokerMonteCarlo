ccc9fd740c7ab510e04d86e9e33c7590e1a8ee13
corbinbaker <corbinbaker@mymail.mines.edu>
2018-04-20 16:54:03 -0600

    main.cpp finished

--- PokerMonteCarlo/PokerMonteCarlo/Game.cpp
+++ PokerMonteCarlo/PokerMonteCarlo/Game.cpp
@@ -4,6 +4,10 @@
 
 
 bool Game::playGame(Player p0, Player p1, int &chips0, int &chips1, bool reportFlag) {
+	/*TODO: Plays a game between a player of type p0 and p1.   Chips is the number of chips 
+	each player respectively has at start and end of the game.   reportFlag is just a flag
+	to turn on and off I/O within the game (so you can do Monte Carlo runs without a lot of output).  
+	The method returns true if someone wants to quit the program.*/
 	while (true) {
 
 	}
--- PokerMonteCarlo/PokerMonteCarlo/main.cpp
+++ PokerMonteCarlo/PokerMonteCarlo/main.cpp
@@ -4,6 +4,13 @@
 
 int main()
 {
-	//TODO:
+	Game g;
+	HumanPlayer hp;
+	AlphaPlayer ap;
+	int hpChips = 100;
+	int apChips = 100;
+	bool IO = true;
+	g.playGame(hp, ap, hpChips, apChips, IO);
+
 	return 0;
 }
