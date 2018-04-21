47e9020b0bab05c3b8804b62c675fc2f51d34373
corbinbaker <corbinbaker@mymail.mines.edu>
2018-04-20 18:59:27 -0600

    comment edit for alphaplayer

--- PokerMonteCarlo/PokerMonteCarlo/AlphaPlayer.cpp
+++ PokerMonteCarlo/PokerMonteCarlo/AlphaPlayer.cpp
@@ -7,7 +7,7 @@
 
 
 int AlphaPlayer::getBet(Hand opponent, BetHistory bh, int bet2player, bool canRaise, int pot) {
-	/*TODO: This is a derived class of Player that evaluates domain information for the current AI player and decides on a player bet.  
+	/*This is a derived class of Player that evaluates domain information for the current AI player and decides on a player bet.  
 	This uses the getBet() method. The Alpha AI should use the attached rules in deciding the bet for the AI player.   */
 
 	if (opponent.evaluateHand() > this->getHand().evaluateHand()) { //if human has better hand, fold
